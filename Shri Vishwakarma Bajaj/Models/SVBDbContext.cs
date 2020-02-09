da72d2a94995b2a26f94adb3dc7fe1ccd2667e64
VIVEK KUMAR SHARMA <vivek.svit2009@gmail.com>
2020-02-09 10:28:18 +0530

    Added Entity  Framework

---
+++ Shri Vishwakarma Bajaj/Models/SVBDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shri_Vishwakarma_Bajaj.Models
+{
+    public class SVBDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+}
